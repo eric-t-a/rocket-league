7391292177939792c892e00640b86549f87a50ee
Eric Agostinho <ericagostinho@Erics-MacBook-Air.local>
2024-04-11 10:21:00 -0300

    removing excessive comments

--- main.js
+++ main.js
@@ -8,44 +8,21 @@ import { threeToCannon, ShapeType } from 'three-to-cannon';
 import World from './world';
 import Car from './car';
 
-// ============
-// part 0
-// set up Three.js scene with axis helper
-// ============
+
 const world = new SceneInit('myThreeJsCanvas');
 world.initialize();
 world.animate();
-// const axesHelper = new THREE.AxesHelper(8);
-// world.scene.add(axesHelper);
-
-// ============
-// part 1
-// set up world physics with gravity
-// ============
-
-
-// create a ground body with a static plane
 
 const cannonWorld = new World(world);
 cannonWorld.initialize();
 const physicsWorld = cannonWorld.physicsWorld;
 
 
-// add a green wireframe to each object and visualize the physics world
 const cannonDebugger = new CannonDebugger(world.scene, physicsWorld);
-
-
-// ============
-// part 2
-// add base vehicle body
-// reference: https://github.com/pmndrs/cannon-es/blob/master/examples/rigid_vehicle.html
-// ============
-
 
 
 const car = new Car(world, physicsWorld);
 car.initialize()
-
 
 
 const animate = () => {
